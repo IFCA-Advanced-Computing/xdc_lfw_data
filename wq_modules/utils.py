a287cf218c0c2e8fc4a0dc5eba217616bfee1fca
Fernando Aguilar <ferag@live.com>
2018-12-28 08:32:31 +0000

    Cosas de Dani

--- wq_modules/utils.py
+++ wq_modules/utils.py
@@ -163,120 +163,6 @@ def unzip_zipfile(local_filename, date_path):
     os.remove(local_filename)
 
 
-def convert_lat_long(lon, lat, mx=None, my=None):
-    """
-    Convert degrees to meters
-    Link : https://en.wikipedia.org/wiki/Geographic_coordinate_system#Expressing_latitude_and_longitude_as_linear_units
-
-    Parameters
-    ----------
-    lon, lat : np.array, shape(N)
-        Arrays of latitudes and longitudes (in degrees)
-    mx, my : float
-        Origin of the map (in degrees).
-        Default is np.amin(lon), np.amin(lat)
-
-    Returns
-    -------
-    lon, lat : np.array, shape(N)
-        Arrays of latitudes and longitudes (in meters)
-    """
-    if mx is None: mx = np.amin(lon)
-    if my is None: my = np.amin(lat)
-
-    def cos(x): return np.cos(np.radians(x))
-
-    mean_lat = np.mean(lat)
-    x_factor = 111132.92 - 559.82 * cos(2*mean_lat) + 1.175 * cos(4*mean_lat) - 0.0023 * cos(6*mean_lat)
-    y_factor = 111412.84 * cos(mean_lat) - 93.5 * cos(3*mean_lat) + 0.118 * cos(5*mean_lat)
-
-    x = (lon - mx) * x_factor
-    y = (lat - my) * y_factor
-    return x, y
-
-
-def download_zip(zip_file_url, output_path):
-    """
-    Sub-function used on: get_sentinel2_raw() and get_landsat8_raw()
-    Download and unzip the earth engine data with the url of the file
-    Inputs:
-        zip_file_url : earth engine url of file
-        output_path : path to download and save
-    """
-    try:
-        r = requests.get(zip_file_url, stream=True)
-        print(r.status_code)
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall(output_path)
-    except requests.exceptions.RequestException as e:  # This is the correct syntax
-       print(e)
-
-
-def tiff_to_netCDF(files_path):
-    """
-    Sub-function used on: get_sentinel2_raw() and get_landsat8_raw()
-    Transform GTIFF files (bands of satellite) to netCDF
-    Inputs:
-        files_path: path of the folder that contains the files
-    """
-    files = os.listdir(files_path)
-    for f in files:
-        file_path = os.path.join(files_path, f)
-
-        if f.endswith('.tif'):
-
-            #Transform tif to netcdf file
-            band_name = f.split('.')[-2]
-            netcdf_path = os.path.join(files_path, '{}.nc'.format(band_name))
-            os.system('gdal_translate -of netCDF {} {}'.format(file_path, netcdf_path))
-
-        os.remove(file_path)
-
-
-def create_netCDF(path, mask, lat, lon, name):
-    """
-    Sub-function used on: cloud.cloud_mask.
-    Create a NetCDF file with mask: clouds, water, ...
-    """
-    #create de netCDF4 file
-    ncfile = Dataset(os.path.join(path, "{}.nc".format(name)),"w", format='NETCDF4_CLASSIC') #'w' stands for write
-
-    ncfile.createDimension('lat', len(lat))
-    ncfile.createDimension('lon', len(lon))
-
-    latitude = ncfile.createVariable('lat', 'f4', ('lat',))
-    longitude = ncfile.createVariable('lon', 'f4', ('lon',))  
-    Band1 = ncfile.createVariable('Band1', 'f4', ('lat', 'lon'))
-
-    ncfile.description = "mask"
-    ncfile.history = "Created" + dt.datetime.today().strftime("%m/%d/%Y")
-    ncfile.source = "netCDF4 python module"
-
-    latitude[:] = lat
-    longitude[:] = lon
-    Band1[:,:] = np.ones((len(lat), len(lon))) * mask * 255
-
-    ncfile.close
-
-
-def check_corner(image, date_path, query_dict):
-    """
-    Download sample to check if it is a corner, and therefore the image is unusable.
-    """
-    band = image.select('B1')
-    band_url = band.getDownloadURL(query_dict)
-    download_zip(band_url, date_path)
-
-    tif_file = [f for f in  os.listdir(date_path) if f.endswith('.tif')]
-    arr = imageio.imread(os.path.join(date_path, tif_file[0]))
-    corner = False
-    if (arr == 0).any():
-        corner = True
-    shutil.rmtree(date_path, ignore_errors=True)
-
-    return corner
-
-
 def load_bands(datepath, band_list):
     """
     Retrieve a dict of band arrays from a date path
@@ -291,45 +177,138 @@ def load_bands(datepath, band_list):
     return longitude, latitude, band_dict
 
 
-def metadata_file(mission, sat_list):
+def cut_tiff_image(date_path, region):
+
+    coord = config.regions[region]['coordinates']
+
+    up_left = utm.from_latlon (coord['N'], coord['W'])
+    down_right = utm.from_latlon (coord['S'], coord['E'])
+
+    files = os.listdir(date_path)
+    for f in files:
+        if f.endswith('.TIF'):
+
+            band_path = os.path.join(date_path, f)
+
+            band = f.split('_')[-1]
+            new_band_path = os.path.join(date_path, band)
+
+            os.system('gdal_translate -projwin {} {} {} {} {} {}'.format(up_left[0], up_left[1], down_right[0], down_right[1], band_path, new_band_path))
+
+            if os.path.isfile(new_band_path):
+                os.remove(band_path)
+
+
+def tiff_to_netcdf(date_path):
+
+    files = os.listdir(date_path)
+    for f in files:
+        if f.endswith('.TIF'):
+            band_path = os.path.join(date_path, f)
+
+            name = f.split('.')[0]
+            netcdf_path = os.path.join(date_path, '{}.nc'.format(name))
+
+            os.system('gdal_translate -of netCDF {} {}'.format(band_path, netcdf_path))
+
+            if os.path.isfile(netcdf_path):
+                os.remove(band_path)
+
+
+def create_netCDF(path, mask, lat, lon, name):
     """
-    create a file with the metadata
+    Sub-function used on: cloud.cloud_mask.
+    Create a NetCDF file with mask: clouds, water, ...
     """
-    if mission == 'Sentinel-2':
-        metadata = {'id': sat_list.getInfo()[0]['id'],
-                    'file': sat_list.getInfo()[0]['properties']['DATATAKE_IDENTIFIER'],
-                    'cloud_coverage': sat_list.getInfo()[0]['properties']['CLOUD_COVERAGE_ASSESSMENT'],
-                    'SENSING_ORBIT_DIRECTION': sat_list.getInfo()[0]['properties']['SENSING_ORBIT_DIRECTION']
-                    }
-    elif mission == 'Landsat8':
-        metadata = {'id': sat_list.getInfo()[0]['id'],
-                    'date': sat_list.getInfo()[0]['properties']['DATE_ACQUIRED'],
-                    'cloud_coverage': sat_list.getInfo()[0]['properties']['CLOUD_COVER'],
-                    'category': sat_list.getInfo()[0]['properties']['COLLECTION_CATEGORY']
-                    }
-    return metadata
+    #create de netCDF4 file
+    new_name = name
+    ncfile = Dataset(os.path.join(path, "{}.nc".format(new_name)),"w", format='NETCDF4_CLASSIC') #'w' stands for write
+
+    ncfile.createDimension('lat', len(lat))
+    ncfile.createDimension('lon', len(lon))
+
+    latitude = ncfile.createVariable('lat', 'f4', ('lat',))
+    longitude = ncfile.createVariable('lon', 'f4', ('lon',))  
+    Band1 = ncfile.createVariable('Band1', 'f4', ('lat', 'lon'))
+
+    ncfile.description = "mask"
+    ncfile.history = "Created" + dt.datetime.today().strftime("%m/%d/%Y")
+    ncfile.source = "netCDF4 python module"
+
+    latitude[:] = lat
+    longitude[:] = lon
+    Band1[:,:] = mask
+
+    ncfile.close
 
 
-def check_file(path):
-    try:
-        with open(os.path.join(path, 'downloaded_files.json')) as data_file:    
-            json.load(data_file)
-    except:
-        if not (os.path.isdir(path)):
-            os.mkdir(path)
-        dictionary = {"Sentinel-2": {"CdP": [], "Sanabria": [], "Castro de las Cogotas": []},
-                    "Landsat 8": {"CdP": [], "Sanabria": [], "Castro de las Cogotas": []}}
-        with open(os.path.join(path, 'downloaded_files.json'), 'w') as outfile:                
-            json.dump(dictionary, outfile)
+def DN_to_reflectance(date_path):
 
+    coefs = {"mult": {}, "add": {}}
+    files = os.listdir(date_path)
 
-def get_pixel_area(datepath, band):
-    """
-    Compute the new pixel area after the lat_long --> meters conversion
-    """
+    for f in files:
+        if f.endswith('MTL.txt'):
+            textfile = f
 
-    lon, lat, data = load_bands(datepath, band)
-    x, y = convert_lat_long(lon, lat)
-    scale_x = np.mean(x[1:]- x[:-1])
-    scale_y = np.mean(y[1:]- y[:-1])
-    return scale_x * scale_y
+    with open(os.path.join(date_path, textfile)) as infile:
+        for line in infile:
+
+            if line.startswith("    REFLECTANCE_ADD"):
+                l = line.split('=')
+                value = float(l[-1])
+                name = l[0].split('_')[-2]+l[0].split('_')[-1]
+                name = name.replace(' ', '')
+
+                coefs["add"][name] = value
+
+            elif line.startswith("    REFLECTANCE_MULT"):
+                l = line.split('=')
+                value = float(l[-1])
+                name = l[0].split('_')[-2]+l[0].split('_')[-1]
+                name = name.replace(' ', '')
+
+                coefs["mult"][name] = value
+
+            elif line.startswith('    SUN_ELEVATION'):
+                l = line.split('=')
+                value = float(l[-1])
+                name = l[0].split('_')[1]
+                name = name.replace(' ', '')
+
+                coefs[name] = value
+
+    pansharpen_bands = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7']
+    band_dict = {}
+
+    for b in pansharpen_bands:
+
+        band = Dataset(os.path.join(date_path, '{}.nc'.format(b)))
+        band_dict[b] = band.variables['Band1'][:] # band array
+
+        lat = band.variables['y'][:]
+        lon = band.variables['x'][:]
+
+        if b =='B1':
+            reflectance = coefs['mult']['BAND1'] * band_dict['B1'].data + coefs['add']['BAND1']
+            reflectance = np.round(reflectance / np.sin(coefs['ELEVATION']), decimals = 4)
+        elif b =='B2':
+            reflectance = coefs['mult']['BAND2'] * band_dict['B2'].data + coefs['add']['BAND2']
+            reflectance = np.round(reflectance / np.sin(coefs['ELEVATION']), decimals = 4)
+        elif b =='B3':
+            reflectance = coefs['mult']['BAND3'] * band_dict['B3'].data + coefs['add']['BAND3']
+            reflectance = np.round(reflectance / np.sin(coefs['ELEVATION']), decimals = 4)
+        elif b =='B4':
+            reflectance = coefs['mult']['BAND4'] * band_dict['B4'].data + coefs['add']['BAND4']
+            reflectance = np.round(reflectance / np.sin(coefs['ELEVATION']), decimals = 4)
+        elif b =='B5':
+            reflectance = coefs['mult']['BAND5'] * band_dict['B5'].data + coefs['add']['BAND5']
+            reflectance = np.round(reflectance / np.sin(coefs['ELEVATION']), decimals = 4)
+        elif b =='B6':
+            reflectance = (coefs['mult']['BAND6'] * band_dict['B6'].data) + coefs['add']['BAND6']
+            reflectance = np.round(reflectance / np.sin(coefs['ELEVATION']), decimals = 4)
+        elif b =='B7':
+            reflectance = (coefs['mult']['BAND7'] * band_dict['B7'].data) + coefs['add']['BAND7']
+            reflectance = np.round(reflectance / np.sin(coefs['ELEVATION']), decimals = 4)
+
+        create_netCDF(date_path, reflectance, lat, lon, b)
