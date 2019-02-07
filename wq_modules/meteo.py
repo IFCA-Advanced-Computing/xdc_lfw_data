5f07742f405e1c9c66aecf07cc32aaa8556067d5
Fernando Aguilar <ferag@live.com>
2019-02-07 13:08:13 +0100

    Update meteo.py
    Updated general name for meteo file

--- wq_modules/meteo.py
+++ wq_modules/meteo.py
@@ -166,16 +166,10 @@ class Meteo:
         return salidaInformacion
 
     def get_meteo(self):
-        #onedata mode
-        if (config.onedata_mode == 1):
-            datasets_path = '/onedata/' + config.onedata_user + '/' + config.onedata_space + '/' + config.download_datasets
-        else:
-            datasets_path = '.' + config.download_datasets
-   
-        self.general_name = datasets_path + '/' + self.region + '/' + "meteo_"+self.inidate.strftime('%Y-%m-%d')+"_"+self.enddate.strftime('%Y-%m-%d')
-
+        self.general_name = self.path + '/' + self.region + '/' + "meteo_"+self.inidate.strftime('%Y-%m-%d')+"_"+self.enddate.strftime('%Y-%m-%d')
         self.station = self.find_station() #TODO add lat/lon
         print(self.station)
         tt=self.datosEstacion()
-        metadata_gen.metadata_gen("meteo_"+self.inidate.strftime('%Y-%m-%d')+"_"+self.enddate.strftime('%Y-%m-%d')+'.csv',self.inidate.strftime('%Y-%m-%d'),self.enddate.strftime('%Y-%m-%d'),self.region,str(self.lat),str(self.lon),self.params)
+        if (config.onedata_mode == 1):
+            metadata_gen.metadata_gen("meteo_"+self.inidate.strftime('%Y-%m-%d')+"_"+self.enddate.strftime('%Y-%m-%d')+'.csv',self.inidate.strftime('%Y-%m-%d'),self.enddate.strftime('%Y-%m-%d'),self.region,str(self.lat),str(self.lon),self.params)
         return {"output": self.general_name + ".csv"}
